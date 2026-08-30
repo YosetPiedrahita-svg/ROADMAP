8c2e76919b76d35f41320d7e19fc65a214cdaade
yosetPiedrahita <yosetalfonsopiedrahita@gmail.com>
2026-08-30 17:34:47 -0500

    Reemplazar contenido de master con el de users"

--- app/(auth)/home/[id]/page.tsx
+++ app/(auth)/home/[id]/page.tsx
@@ -1,5 +1,7 @@
 //* pagina que se renderiza al estar logeado el usario
 const idPage = () => {
-  return <main>hola</main>;
+  return <main>hola mundo</main>;
 };
 export default idPage;
+
+//? cambio para permitir el comit
