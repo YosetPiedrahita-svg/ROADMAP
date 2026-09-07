14423272f1ab0c086f3e5b26ccca4daa72a1e86a
yosetPiedrahita <yosetalfonsopiedrahita@gmail.com>
2026-09-07 12:15:18 -0500

    refactor: loading spinner

--- app/(auth)/home/[id]/profile/loading.tsx
+++ app/(auth)/home/[id]/profile/loading.tsx
@@ -1,4 +1,14 @@
+import { Loader, Loader2 } from "lucide-react"; // Si usas lucide-react (común con Shadcn)
+
 const ProfileLoading = () => {
-  return <div>ProfileLoading</div>;
+  return (
+    <main className="flex h-screen w-full flex-col items-center justify-center gap-2 bg-background">
+      <Loader className="h-8 w-8 animate-spin text-muted-foreground" />
+      <p className="text-sm font-semibold tracking-wide text-muted-foreground animate-pulse">
+        Cargando perfil...
+      </p>
+    </main>
+  );
 };
+
 export default ProfileLoading;
